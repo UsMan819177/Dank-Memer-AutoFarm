c02c4ce7c1ee9bf56dde12f6884a89b2a08ec0b5
UsMan819177 <103477999+UsMan819177@users.noreply.github.com>
2022-04-12 12:41:06 +0800

    Create keep_alive.py

---
+++ keep_alive.py
@@ -0,0 +1,16 @@
+from flash import Flask
+from threading import Thread
+
+app = Flask('')
+
+@app.route('/')
+def home():
+return "Bot Is Working!"
+
+def run():
+app run(host='0.0.0.0',port=8080)
+
+def keep_alive():
+
+t = Thread(target=run)
+t.start()
